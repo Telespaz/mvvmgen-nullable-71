ce77bb8d2e1182314e49f18170e56be78c02cdf9
Steffen Wüst <steffen.wuest@telespazio.de>
2023-08-14 08:45:46 +0200

    sample project

---
+++ mvvmgen-nullable-71-sample/mvvmgen-nullable-71-sample/SampleViewModel.cs
@@ -0,0 +1,26 @@
+﻿using MvvmGen;
+
+namespace mvvmgen_nullable_71_sample
+{
+    [ViewModel]
+    public partial class SampleViewModel
+    {
+        [Property]
+        private string m_text;
+
+        [Property]
+        private int m_counter;
+
+        partial void OnInitialize()
+        {
+            Text = "Test";
+            Counter = 0;
+        }
+
+        [Command]
+        public void Increase()
+        {
+            Counter++;            
+        }
+    }
+}
